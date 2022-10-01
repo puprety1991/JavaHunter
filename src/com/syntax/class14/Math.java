5a2ab8240fa9bf487d8c1fae721f93276cd5e1c3
Puskar Uprety <upretypuskar@gmail.com>
2022-10-01 13:29:43 -0400

    methods

---
+++ src/com/syntax/class14/Math.java
@@ -0,0 +1,25 @@
+package com.syntax.class14;
+
+public class Math {
+    void printFiboSeries(int size){
+
+        int previous=0;
+        int currentNumber=1;
+        int nextNum=0;
+        System.out.println("previous number: "+previous);
+        System.out.println("current number: "+currentNumber);
+        for(int i=0; i<size;i++) {
+            nextNum=previous+currentNumber;
+            System.out.print(nextNum+" ");
+            previous=currentNumber;
+            currentNumber=nextNum;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Math obj=new Math(); // Creating an object of the Math class
+        obj.printFiboSeries(5);
+        obj.printFiboSeries(10);
+    }
+}
