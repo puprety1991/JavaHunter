8e0da69709ec9bdef928c505eabdb9581915bf58
Puskar Uprety <upretypuskar@gmail.com>
2022-10-02 11:30:36 -0400

    adding void  behavior

--- src/com/syntax/class14/PalindromeTask.java
+++ src/com/syntax/class14/PalindromeTask.java
@@ -9,20 +9,33 @@ public class PalindromeTask {
         for(int i=word.length()-1;i>=0; i--){
             reverse+=word.charAt(i);
         }
-        if(reverse.equals(word)){
+        if(reverse.equalsIgnoreCase(word)){
             return true;
         }else{
             return false;
         }
 
     }
+                //using void behaviour
+    void isPalindrome(String name){
+        name = name.toLowerCase();//if we convert to lower it will save in name
+        StringBuilder word = new StringBuilder(name);
+        word.reverse();
+        if(name.equals(word.toString())){
+            System.out.println(name+ " is palindrome");
+        }else{
+            System.out.println(name+" is not palindrome");
+        }
+    }
 
     public static void main(String[] args) {
         PalindromeTask toCheck = new PalindromeTask();
-        if(toCheck.palidrome("anna")){
+        if(toCheck.palidrome("Anna")){
             System.out.println("This is palindrome");
         }else{
             System.out.println("This is not a palindrome");
         }
+                    //calling from void
+        toCheck.isPalindrome("AbA");
     }
 }
