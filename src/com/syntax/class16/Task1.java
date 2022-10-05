0ea2bfbfc0af308ef1b7f84bb4b09c711efe00d3
Puskar Uprety <upretypuskar@gmail.com>
2022-10-05 14:18:50 -0400

    make changes

--- src/com/syntax/class16/Task1.java
+++ src/com/syntax/class16/Task1.java
@@ -6,7 +6,9 @@ public class Task1 {
     sum of all elements from that array. Method should be visibly only within
     same package and accessible by the creating an instance of the class.
      */
+    //static int[] array = {4,6,3,6,4,7,7,5,6};
      int sumNum(int [] array){
+
         int sum = 0;
         for(int i=0;i<array.length;i++){
             sum+=array[i];
@@ -16,6 +18,8 @@ public class Task1 {
 
     public static void main(String[] args) {
         Task1 math = new Task1();
+
+        //System.out.println(math.sumNum(array));
         System.out.println(math.sumNum(new int[]{5, 6, 7, 8, 3}));
     }
 }
