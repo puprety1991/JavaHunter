4c35f3de14bed6f1010d06d39ae6576f67fbd967
Puskar Uprety <upretypuskar@gmail.com>
2022-10-13 21:43:32 -0400

    review

---
+++ src/com/syntax/review/class08/Country.java
@@ -0,0 +1,20 @@
+package com.syntax.review.class08;
+
+public class Country {
+    public String name;
+    protected String capital;
+    int population;
+    private String president;
+
+}
+class Peru extends Country{
+
+}
+class CountryTester{
+    public static void main(String[] args) {
+        Peru peru = new Peru();
+        peru.capital = "Lima";
+        peru.population=22000000;
+        //peru.president --> not accessible because of private access modifiers.
+    }
+}
