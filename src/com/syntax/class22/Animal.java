16ee49201279e24a9eaf86ae73c4f517c3805016
Puskar Uprety <upretypuskar@gmail.com>
2022-10-15 11:05:18 -0400

    review

---
+++ src/com/syntax/class22/Animal.java
@@ -0,0 +1,32 @@
+package com.syntax.class22;
+
+public class Animal {
+    void eat(){
+        System.out.println("Animals usually eat plants");
+    }
+    void sleep(){
+        System.out.println("animals sleep for 8 to 10 hours");
+    }
+}
+class Panda extends Animal{
+    void eat(){
+        System.out.println("Pandas eat Bamboos");
+    }
+    // @Override is an annotation that tells java to check for extra rule
+    // like if we are properly overriding a method or not
+    @Override
+    void sleep(){
+        System.out.println("Pandas sleep for 14 hours");
+    }
+}
+class Cat extends Animal{
+    void eat(){
+        System.out.println("Cat eat Fish");
+    }
+}
+class AnimalTester{
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+    }
+}
