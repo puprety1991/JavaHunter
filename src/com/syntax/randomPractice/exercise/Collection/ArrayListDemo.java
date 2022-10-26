ffb3afc8bad1785efc342500f934707eb831d1bc
Puskar Uprety <upretypuskar@gmail.com>
2022-10-26 19:02:46 -0400

    arraylist

---
+++ src/com/syntax/randomPractice/exercise/Collection/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.syntax.randomPractice.exercise.Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer>integerArrayList = new ArrayList<>();
+        integerArrayList.add(10);
+        integerArrayList.add(20);
+        integerArrayList.add(564);
+        System.out.println(integerArrayList);
+    }
+}
